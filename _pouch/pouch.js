478a801d3baedd4339c71b0eeb709eada02b7a56
roobottom <roo@roodesign.co.uk>
2018-01-30 22:38:17 +0000

    refactoring pouch

--- _pouch/pouch.js
+++ _pouch/pouch.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const settings = require('./_settings.js')
+const site = require('./_site.js')
 
 //filesystem
 const path = require('path')
@@ -9,13 +9,14 @@ const fs = require('fs-extra')
 
 //utils
 const _ = require('lodash')
+const util = require('util')
 
 //frontmatter
 const frontmatter = require('front-matter')
 
 //nunjucks
 const nunjucks = require('nunjucks')
-const nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(settings.nunjucksPath),
+const nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(site.nunjucksPath),
   {
     autoescape: false,
     noCache:true
@@ -33,172 +34,67 @@ const md = new Remarkable('full',{
 //time
 const moment = require('moment')
 
-function getObjectsFromSource(source) {
-
-  //loop through each item in this source
-  for(let item of settings[source]) {
-    //get list of files from this item's input glob
-    let files = glob.sync(item.input)
-
-    //build local object of item contents
-    let objects = []
-    for(let file of files) {
-      objects.push(readFileContents(file))
-    }
-
-    //attach the source's items to the item object
-    item.items = objects
-
-  }
-
-  //return the updated source
-  return settings[source]
-}
-
-//returns an object containing `attributes` and `content` objects
-function readFileContents(file) {
-
-  let data = fs.readFileSync(file,'utf8')
-
-  //read frontmatter
-  let fm = frontmatter(data)
-
-  //render nunjcuks tags
-  let renderedContent = nunjucksEnv.renderString(fm.body,fm.attributes)
-
-  //render md -> html
-  renderedContent = md.render(renderedContent)
-
-  //get filename
-  let filePath = path.parse(file)
 
 
-  let returnObj = {}
-  returnObj.attributes = fm.attributes
-  returnObj.id = filePath.name
-  returnObj.content = renderedContent
 
-  return returnObj
 
-}
+function addItemsToCollections(collections) {
+  for(let collection of collections) {
 
-function renderFileWithTemplate(template,data) {
-  return nunjucksEnv.render(template,data)
-}
+    for(let item of site[collection]) {
 
-function sortItems(items,sortby) {
-  return items.sort((a,b) => {
-    return b[sortby] - a[sortby]
-  })
-}
+      //get list of files from this item's input glob
+      let files = glob.sync(item.input)
 
-function getTags(items) {
-  let tags = []
-  for(let item of items) {
-    if(item.attributes.tags) {
-      tags.push(item.attributes.tags)
-    }
-  }
+      //build local object of item contents
+      let objects = []
+      for(let file of files) {
 
-  //clean tags list
-  tags = _.flatten(tags)
-  for(let tag of tags) {
-    tag = tag.toLowerCase()
-  }
+        let data = fs.readFileSync(file,'utf8')
 
-  //count unique tags
-  let uniqueTagCount = []
-  tags.forEach(function(n) {
-    uniqueTagCount[n] = (uniqueTagCount[n] || 0)+1
-  })
+        //read frontmatter
+        let fm = frontmatter(data)
 
-  //create tagsObject
-  let tagsObject = []
-  for(let key in uniqueTagCount) {
-    tagsObject.push({
-      name: key,
-      count: uniqueTagCount[key]
-    })
-  }
+        //render nunjcuks tags
+        let renderedContent = nunjucksEnv.renderString(fm.body,fm.attributes)
 
-  return tagsObject
-}
+        //render md -> html
+        renderedContent = md.render(renderedContent)
 
-let pouchGo = function() {
+        //get filename
+        let filePath = path.parse(file)
 
-  let site = {
-    pages: getObjectsFromSource('pages'),
-    blogs: getObjectsFromSource('blogs')
-  }
 
-  //update the site object with meta from blogs
-  for(let key in site.blogs) {
+        let returnObj = {}
+        returnObj.attributes = fm.attributes
+        returnObj.filename = filePath.name
+        returnObj.content = renderedContent
 
-    //if blog has a sort by key
-    if(site.blogs[key].sortBy) {
-      site.blogs[key].items = sortItems(site.blogs[key].items,site.blogs[key].sortBy)
-    }
-
-    //if blog tags, get tags:
-    if(site.blogs[key].tags) {
-      site.blogs[key].tags.items = getTags(site.blogs[key].items)
-    }
-
-  }
-
-  //render pages
-  for(let items of site.pages) {
-
-    for(let item of items.items) {
-
-      //attach meta
-      if(items.tags) {
-        item.attributes.tags = items.tags
+        objects.push(returnObj)
       }
 
-      let renderedFile = renderFileWithTemplate(items.template,{
-        content: item.content,
-        page: item.attributes,
-        site: site
-      })
-
-      fs.ensureFileSync(items.output)
-      fs.writeFileSync(items.output,renderedFile)
+      //push local objects back into site
+      item.items = objects
 
     }
 
   }
+}
 
-
-
-  //render blogs
-  for(let blog of site.blogs) {
-
-    //for each item in this blog
-    for(let item of blog.items) {
-
-      let renderedFile = renderFileWithTemplate(blog.template,{
-        content: item.content,
-        page: item.attributes
-      })
-
-      let output = blog.output + '/' + item.id + '/index.html'
-
-      fs.ensureFileSync(output)
-      fs.writeFileSync(output,renderedFile)
-
+function sortCollections(collections) {
+  for(let collectionName of collections) {
+    for(let collection of site[collectionName]) {
+      if(collection.sortBy) {
+        collection.items.sort((a,b) => {
+          return b.attributes[collection.sortBy] - a.attributes[collection.sortBy]
+        })
+      }
     }
-
   }
-
-  return true
 }
 
-//async wrapper
-let pouchAsync = function(cb) {
-  pouchGo()
-  cb()
-}
+let collections = ['blogs','pages']
+addItemsToCollections(collections)
+sortCollections(collections)
 
-exports.pouchGo = pouchGo
-exports.pouchAsync = pouchAsync
+console.log(util.inspect(site, false, null))
