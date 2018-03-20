bbd0aef9e5734c5b40a84f4b2948f65178b1fa36
roobottom <roo@roodesign.co.uk>
2018-03-20 10:20:18 +0000

    started building

--- docs/js/header.js
+++ docs/js/header.js
@@ -2,49 +2,101 @@ class wobbleOverlay {
   constructor(element) {
     this.element = element
     this.path = element.querySelectorAll('path')
-    this.points = 12
-    this.curve = 20 //curyness of curve
-    this.amplitude = 80 //height of 2 curves
+    this.points = 2
+    this.phase = 10 //phase angle
+    this.amplitude = 80 //height of 2 phases
     this.midpoint = this.amplitude / 2
     this.width = 600 //fit wave to this width (viewBox width)
-    this.wavelength = this.width / this.points //the length of each curve
+    this.wavelength = this.width / this.points //the length of each phase
   }
 
-  animate() {
+  run(amplitude,repeat,reducer,direction) {
 
-    var direction = 1 //forward = 1, backwards = -1
-    var states = []
-    for(var i = 0; i < this.amplitude; i++) {
-      states.push(this.draw(i))
-      var newPath = document.createElementNS("http://www.w3.org/2000/svg", 'path')
-      newPath.setAttribute('d',this.draw(i))
-      newPath.setAttribute('stroke-opacity','0.1')
-      newPath.setAttribute('stroke','red')
-      newPath.setAttribute('fill','transparent')
-      this.element.appendChild(newPath)
+    var that = this
+    if(!direction) direction = 1
+    if(!reducer) reducer = 20
+
+    that.animate({
+      duration: 500,
+      timing(t) {
+        return t < 0.5
+            ? 0.5 * (1.0 - Math.sqrt(1.0 - 4.0 * t * t))
+            : 0.5 * (Math.sqrt((3.0 - 2.0 * t) * (2.0 * t - 1.0)) + 1.0);
+      },
+      draw(progress) {
+
+        var adjustedAmplitude
+        adjustedAmplitude = direction * Math.floor(amplitude - ((progress * 2) * amplitude))
+
+        drawPath(adjustedAmplitude)
+      },
+      cb() {
+
+        if(repeat != 0) {
+          repeat = repeat - 1;
+
+          if(direction == 1) {
+            direction = -1
+          }
+          else {
+            direction = 1
+          }
+
+          amplitude = amplitude - (repeat * reducer )
+
+          that.run(amplitude,repeat,reducer,direction)
+        }
+
+      }
+    })
+
+    var drawPath = (n) => {
+      this.path[0].setAttribute('d',this.drawPath(n))
     }
 
-    console.log(states)
+  }
 
+  animate({timing, draw, duration,cb}) {
+
+    let start = performance.now();
+    let i = 0;
+    requestAnimationFrame(function animate(time) {
+      // timeFraction goes from 0 to 1
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+      if (timeFraction == 1) {
+        cb()
+      }
+      else {
+        // calculate the current animation state
+        let progress = timing(timeFraction)
+        draw(progress); // draw it
+        requestAnimationFrame(animate);
+      }
+
+
+    });
   }
 
 
-  draw(amplitude) {
+  drawPath(amplitude) {
     var str = `M 0, ${this.midpoint}`
     var up = true
 
-    var x1delta = (this.wavelength / 2) + this.curve
-    var x2delta = (this.wavelength / 2) - this.curve
+    var x1delta = (this.wavelength / 2) + this.phase
+    var x2delta = (this.wavelength / 2) - this.phase
 
     for(var i = 0; i < this.points; i++) {
 
       var x = (i + 1) * this.wavelength
       var y = this.midpoint
 
+      //calculate the phase angle
       var x1 = x - x1delta
       var x2 = x - x2delta
 
-      var y1 = up ? (this.midpoint * 2) - amplitude : amplitude
+      //calculte the hight/depth of this wave
+      var y1 = up ? this.midpoint + (amplitude/2) : this.midpoint - (amplitude/2)
       up = up ? false : true
 
       str += ` C ${x1} ${y1} ${x2} ${y1} ${x} ${y}`
@@ -64,7 +116,7 @@ class wobbleOverlay {
   var sea = document.querySelector('.sea')
   var wobble = new wobbleOverlay(sea);
   //wobble.draw()
-  wobble.animate()
+  wobble.run(80,10)
 
 
 }());
--- source/work/firefly-learning-brand-values.md
+++ source/work/firefly-learning-brand-values.md
@@ -3,7 +3,8 @@ title: Uncovering Firefly Learning’s brand values
 date: 2018-02-01
 org: firefly
 ---
-We knew what we didI lead a project to uncover Firefly's essence. 
+
+We knew *what* we did, less clear was *why* we did it. I lead a project to uncover Firefly's brand values.
 ---
 
 Over the last five years, Firefly Learning has enjoyed tremendous success; we have grown from fifty schools to over five hundred. We now have offices in London, Sydney and Singapore. Our dream to transform learning by empowering all students, teachers and parents is a lot closer today than it was when I joined in September 2011.
